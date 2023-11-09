0094bab79519c4c2b61ea0afdc8068675fafe202
Cody Ellis <46459042+leoisking@users.noreply.github.com>
2023-11-08 18:43:21 -0600

    Weather report.py
    Small changes

--- weather_report.py
+++ weather_report.py
@@ -16,42 +16,39 @@ URL = 'https://api.weather.gov/zones/forecast/NEZ002/forecast'
 
 
 
-    #get the weather information
+#get the weather information
 response = requests.get(URL)
 forecast = response.json()['properties']
 details = forecast['periods']
+
     
+#function to get the weather for the day
+def get_weather():
+    if details:
+        period = details[0]
+        nameInfo = period['name']
+        details2 = period['detailedForecast']
+        n(nameInfo + "\n", details2, duration = 'long', button = tonight ('Tonight'))
 
-if details:
-       
-    period = details[0]
-    nameInfo = period['name']
-    details2 = period['detailedForecast']
-    n(nameInfo + "\n", details2, icon='https://cdn1.iconfinder.com/data/icons/gardening-109/64/24_sunlight_sun_bright_sunshine_daylight_day_star_sun_rays-512.png', duration = 'long', button = 'Tonight')
-
-    # Get the second period
+# function to get tonights weather
 def tonight():
-    response = requests.get(URL)
-    forecast = response.json()['properties']
-    nightTime = forecast['periods']
-    
-    if nightTime:
-    
-        period1 = nightTime[1]
+    if details:
+        period1 = details[1]
         nameInfo1 = period1['name']
         details3 = period1['detailedForecast']
         n(nameInfo1 + "\n", details3, duration = 'long')
-    #button for the notification
+        
+#button for the notification
 buttons = [
     {'activationType': 'protocol', 'arguments':tonight(), 'content': 'Tonight'}]
 
-    # Schedule the function to run every 2 hours
-schedule.cyclic(dt.timedelta(hours=2))
+# Schedule the function to run every 2 hours
+schedule.cyclic(dt.timedelta(hours=2), get_weather)
 schedule.cyclic(dt.timedelta(hours=2), tonight)
 
-    # Keep the script running
+# Keep the script running
 while True:
-    schedule.exec_jobs()
+    schedule.exec_jobs(get_weather, tonight)
     time.sleep(1800)
         
 
